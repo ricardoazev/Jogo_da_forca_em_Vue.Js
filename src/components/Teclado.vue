308a422fa0ab92e074976425cfc68a49696c267a
ricardoazev <ricardometronome@gmail.com>
2021-11-13 11:11:19 -0300

    Primeiro commit

---
+++ src/components/Teclado.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="teclado">
+        
+        <button class="teclado-botao" v-for="(letra, key) in 'abcdefghijlmnopqrstuvwxyz'" :key="key" :disable="verificarLetra(letra)"
+        v-on:click="jogar(letra)"
+        >
+        {{ letra}}
+        </button>
+      
+    </div>
+    
+</template>
+
+<script>
+
+
+export default {
+    name: 'Teclado',
+    props:{
+      Letras: Array,
+      verificarLetra: Function,
+      jogar: Function
+      
+    },
+    data(){
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+    components: {
+        
+    }
+}
+</script>
+
+<style>
+
+.teclado{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 85%;
+    margin-top: 20px;
+}
+
+.teclado-botao{
+    margin: 5px;
+    text-transform: uppercase;
+}
+
+
+
+
+</style>
---
+++ src/components/jogo.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="jogo">
+        <Forca :erros="erros"/>
+
+        <Palavra
+            :palavra="palavra"
+            :dica="dica"
+            :verificarLetra="verificarLetra"
+            :etapa="etapa"/>
+    
+
+         <Teclado
+            v-if="etapa === 'jogo'"
+            :letras="letras"
+            :verificarLetra="verificarLetra"
+            :jogar="jogar"/>
+
+        <Final
+        v-if="etapa != 'jogo'"
+            :etapa="etapa"
+            :texto="etapa === 'ganhador' ? 'Parabéns :)' : 'Tadinho :('"
+            :jogarNovamente="jogarNovamente"
+             />
+
+    </div>
+    
+</template>
+
+<script>
+import Forca from './Forca.vue' 
+import Palavra from './Palavra.vue'
+import Teclado from './Teclado.vue'
+import Final from './Final.vue'
+
+export default {
+    name: 'jogo',
+    props:{
+      erros: Number,
+      palavra: String,
+      dica: String,
+      verificarLetra: Function,
+      etapa: String,
+      letras: Array,
+      jogar: Function,
+      jogarNovamente: Function
+
+    },
+    data(){
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+    components: {
+        Forca,
+        Palavra,
+        Teclado,
+        Final
+
+    }
+}
+</script>
+
+<style>
+.jogo{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+}
+
+
+</style>
